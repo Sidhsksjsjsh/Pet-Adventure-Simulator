35eeaf65a8c777a84da1baaa146361ed3d322abb
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-03-02 14:09:24 +0900

    Update .lua

--- .lua
+++ .lua
@@ -4,7 +4,9 @@ local T1 = wndw:Tab("Main")
 local T2 = wndw:Tab("Egg")
 local T3 = wndw:Tab("Pet Training")
 
+local user = game:GetService("Players").LocalPlayer
 local vendors = {}
+local workspace = game:GetService("Workspace")
 local td = {
          arg = {
              str1 = "1",
@@ -41,10 +43,31 @@ T1:Toggle("Auto claim hourly rewards",false,function(value)
     end
 end)
 
+T1:Toggle("Auto end fight",false,function(value)
+    _G.endf = value
+    while wait() do
+      if _G.endf == false then break end
+      game:GetService("ReplicatedStorage")["Events"]["EndBattle"]:FireServer(user)
+    end
+end)
+
+T1:Toggle("Auto spin",false,function(value)
+    _G.spinwheel = value
+    while wait() do
+      if _G.spinwheel == false then break end
+      game:GetService("ReplicatedStorage")["Events"]["SpinWheelEvent"]:FireServer("Spin")
+      game:GetService("ReplicatedStorage")["Events"]["SpinWheelEvent"]:FireServer("SpinComplete")
+    end
+end)
+
 T1:Button("Claim all battlepass [ Bypass Requirement ]",function()
     for array = 1,20 do
       game:GetService("ReplicatedStorage")["Events"]["UpdateSeasonPass"]:FireServer(array,"Free")
     end
+end)
+
+T1:Button("Join obby",function()
+         game:GetService("ReplicatedStorage")["Events"]["PetTraining"]:FireServer("1","Obby")
 end)
 
 T2:Dropdown("Select eggs",vendors,function(value)
@@ -75,9 +98,27 @@ T3:Toggle("Auto collect XP",false,function(value)
     end
 end)
 
+T3:Toggle("Auto click mana",false,function(value)
+    _G.clickmana = value
+    while wait() do
+      if _G.clickmana == false then break end
+      game:GetService("ReplicatedStorage")["Events"]["PetBattleCLIENTtoBACKENDRemoteEvent"]:FireServer("playerClickedForMana")
+    end
+end)
+
 lib:HookFunction(function(method,self,args)
     if self == "PetTraining" and method == "FireServer" then
       td.arg.str1 = tostring(args[1])
       td.arg.str2 = tostring(args[2])
     end
+end)
+
+T1:Toggle("Auto claim XP from pet training",false,function(value)
+         _G.XPpt = value
+end)
+
+workspace["XPParts"].ChildAdded:Connect(function(str)
+        if _G.XPpt == true then
+                  k
+         end
 end)
